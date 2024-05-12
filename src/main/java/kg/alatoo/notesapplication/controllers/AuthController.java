157766b7edbf171dd8dde47329d1d35b619ac212
mohamraheba <raheba.mohammadzahir@alatoo.edu.kg>
2024-05-12 19:41:51 +0600

    access token expiration time

--- src/main/java/kg/alatoo/notesapplication/controllers/AuthController.java
+++ src/main/java/kg/alatoo/notesapplication/controllers/AuthController.java
@@ -1,2 +1,43 @@
-package kg.alatoo.notesapplication.controllers;public class AuthController {
+package kg.alatoo.notesapplication.controllers;
+
+import kg.alatoo.notesapplication.dto.authorization.AuthLoginDTO;
+import kg.alatoo.notesapplication.dto.authorization.JwtDTO;
+import kg.alatoo.notesapplication.entity.RefreshToken;
+import kg.alatoo.notesapplication.services.JwtService;
+import kg.alatoo.notesapplication.services.RefreshTokenService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/auth")
+@RequiredArgsConstructor
+@Slf4j
+public class AuthController {
+
+    private final AuthenticationManager authenticationManager;
+    private final JwtService jwtService;
+    private final RefreshTokenService refreshTokenService;
+
+    @PostMapping("/login")
+    public JwtDTO AuthenticateAndGetToken(@RequestBody AuthLoginDTO authRequestDTO){
+        Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(authRequestDTO.getUsername(), authRequestDTO.getPassword()));
+        if(authentication.isAuthenticated()){
+            RefreshToken refreshToken = refreshTokenService.createRefreshToken(authRequestDTO.getUsername());
+            return JwtDTO.builder()
+                    .accessToken(jwtService.GenerateToken(authRequestDTO.getUsername()))
+                    .token(refreshToken.getToken())
+                    .build();
+
+        } else {
+            throw new UsernameNotFoundException("invalid user request..!!");
+        }
+    }
 }
